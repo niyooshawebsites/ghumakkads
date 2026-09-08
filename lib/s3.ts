83fbbad607cccc086f8a5689f3d3a1b9022bdd8b
niyooshawebsites <niyooshawebsites@gmail.com>
2026-09-08 17:55:11 +0530

    updated env

--- lib/s3.ts
+++ lib/s3.ts
@@ -1,9 +1,9 @@
 import { S3Client } from "@aws-sdk/client-s3";
 
 export const s3Client = new S3Client({
-  region: process.env.AWS_REGION,
+  region: process.env.MY_AWS_REGION,
   credentials: {
     accessKeyId: process.env.MY_AWS_ACCESS_KEY,
-    secretAccessKey: process.env.AWS_SECRET_KEY,
+    secretAccessKey: process.env.MY_AWS_SECRET_KEY,
   },
 });
